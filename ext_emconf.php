32aa7310e1ce883c1c0c9c25e10713410e0d11d5
Philipp Kitzberger <coding@kitze.net>
2021-10-27 15:32:55 +0200

    Drop support for TYPO3 8 & 9

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
+            'typo3' => '10.4.0-11.4.99',
             'news' => '9.0.0-10.99.99',
         ],
         'conflicts' => [],
