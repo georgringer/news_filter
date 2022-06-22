36c9c765a0e5864897534d339b2162b2282ccfa6
Georg Ringer <georg.ringer@gmail.com>
2022-06-22 15:49:44 +0200

    [TASK] Fix constraint

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'mail@ringer.it',
     'state' => 'beta',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.0',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.4.99',
+            'typo3' => '10.4.0-11.5.99',
             'news' => '9.0.0-10.99.99',
         ],
         'conflicts' => [],
