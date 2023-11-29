664877034af578145328cd04468a33629f55c20c
Philipp Kitzberger <coding@kitze.net>
2023-11-29 11:58:39 +0100

    Raise version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,8 +10,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.5.99',
-            'news' => '9.0.0-10.99.99',
+            'typo3' => '10.4.0-12.4.99',
+            'news' => '9.0.0-11.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
